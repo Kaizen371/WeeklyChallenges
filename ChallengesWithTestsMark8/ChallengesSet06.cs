805f6e1e304911dedf8bd0556258238c112751c1
Michaiah Campbell <michaiah@campbell-web.com>
2025-11-23 19:29:35 -0500

    finished all methods for challenge sets 5 and 6

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,7 +8,29 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if(words == null){return false;}
+            bool containsWord = false;
+            string[] caseWords;
+            if (ignoreCase)
+            {
+                
+                caseWords = words.Where(w => w != null).Select(w => w.ToLower()).ToArray();
+                
+                
+            }
+            else
+            {
+                caseWords = words.ToArray();
+                
+            }
+            for (int i = 0; i < caseWords.Count(); i++)
+            {
+                if (caseWords[i] == word)
+                {
+                    containsWord = true;
+                }
+            }
+            return containsWord;
         }
 
         public bool IsPrimeNumber(int num)
@@ -28,17 +51,74 @@ namespace ChallengesWithTestsMark8
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            var charArray = str.ToCharArray();
+            var index = -1;
+            
+           for (int i = 0; i < charArray.Length; i++)
+           {
+               var found = false;
+               for (int j = 0; j < charArray.Length; j++)
+               {
+                   if (i == j)
+                   {
+                       continue;
+                   }
+                   if (charArray[i] == charArray[j])
+                   {
+                       found = true;
+                       break;
+                   }
+                   
+               }
+
+               if (!found)
+               {
+                   index = i;
+               }
+           }
+                
+            return index;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            var currentCount = 1;
+            var maxCount = 1;
+            
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    currentCount++;
+                }
+                else if (numbers[i] != numbers[i + 1])
+                {
+                    currentCount = 1;
+                }
+
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                }
+            }
+            return maxCount;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null || elements.Count == 0 || n < 1)
+            {
+                return Array.Empty<double>();
+            }
+            
+            
+            var result = new double[elements.Count / n ];
+            
+            for (int i = n - 1; i < elements.Count(); i += n)
+            {
+                result[(i / n)] = elements[i];
+            }
+            return result;
         }
     }
 }
--- ChallengesWithTestsMark8/obj/Debug/net9.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net9.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fadbeede5d65ad62307024238eedb1b2f2675519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf9e92723413d6377d8c9065bb6b647d17e0709")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
